fc7de4e023f1a6a317ec15db3d9871f7c1264bf7
laurapugaa <laurademirandapuga@gmail.com>
2025-04-16 00:05:30 -0300

    Commit versão 1 projeto

---
+++ 4299-javascript-projeto-base/index.js
@@ -0,0 +1,13 @@
+import { criarItemDaLista } from "./scripts/criarItemDaLista.js";
+import verificarListaVazia from "./scripts/varificarListaVazia.js";
+const listaDeCompras = document.getElementById("lista-de-compras");
+const botaoAdicionar = document.getElementById("adicionar-item");
+
+botaoAdicionar.addEventListener("click", (evento) => {
+    evento.preventDefault();
+    const itemDaLista = criarItemDaLista();
+    listaDeCompras.appendChild(itemDaLista);
+    verificarListaVazia(listaDeCompras);
+})
+
+verificarListaVazia(listaDeCompras);
